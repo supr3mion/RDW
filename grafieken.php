1ef80e58e196dcf0bf33ca55b6a55d573d74380d
Marthijs1 <75075864+Marthijs1@users.noreply.github.com>
2021-05-24 11:56:12 +0200

    Grafieken Pagina

---
+++ grafieken.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="style.css">
+    <title>Uitgebreide Informatie</title>
+</head>
+<body>
+    <h3>Hier kunt u nog meer informatie vinden, maar dan over alle auto's in Nederland!</h3>
+
+<div class="grafiek"></div>
+<div class="grafiek"></div>
+<div class="grafiek"></div>
+<div class="grafiek"></div>
+<div class="grafiek"></div>
+<div class="grafiek"></div>
+<div class="grafiek"></div>
+<div class="grafiek"></div>
+<div class="grafiek"></div>
+<div class="grafiek"></div>
+<div class="grafiek"></div>
+<div class="grafiek"></div>
+<div class="grafiek"></div>
+<div class="grafiek"></div>
+<div class="grafiek"></div>
+<div class="grafiek"></div>
+<div class="grafiek"></div>
+
+
+
+
+
+</body>
+</html>
